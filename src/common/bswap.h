c1cbc9d7cf6e9aeb9c490afc89e53c71e43de4dd
chys <admin@chys.info>
2010-02-05 07:47:37 +0000

    Fix one problem on big-endian systems

--- src/common/bswap.h
+++ src/common/bswap.h
@@ -33,8 +33,8 @@ inline uint32_t bswap32 (uint32_t x)
 {
 	                              // AABBCCDD
 	x = (x >> 16) | (x << 16);    // CCDDAABB
-	x = ((x >> 8) | (0x00FF00FFu)) | // 00CC00AA
-		((x << 8) | (0xFF00FF00u));  // DD00BB00
+	x = ((x >> 8) & (0x00FF00FFu)) | // 00CC00AA
+		((x << 8) & (0xFF00FF00u));  // DD00BB00
 	return x;
 }
 
@@ -42,11 +42,11 @@ inline uint64_t bswap64 (uint64_t x)
 {
 	                                                       // AABBCCDD EEFFGGHH
 	x = (x >> 32) | (x << 32);                             // EEFFGGHH AABBCCDD
-	x = ((x >> 16) | TIARY_UINT64_C(0x0000ffffu,0x0000ffffu)) | // 0000EEFF 0000AABB
-		((x << 16) | TIARY_UINT64_C(0xffff0000u,0xffff0000u));  // GGHH0000 CCDD0000
+	x = ((x >> 16) & TIARY_UINT64_C(0x0000ffffu,0x0000ffffu)) | // 0000EEFF 0000AABB
+		((x << 16) & TIARY_UINT64_C(0xffff0000u,0xffff0000u));  // GGHH0000 CCDD0000
 	                                                            // GGHHEEFF CCDDAABB
-	x = ((x >> 8) | TIARY_UINT64_C(0x00ff00ffu,0x00ff00ffu)) |  // 00GG00EE 00CC00AA
-		((x << 8) | TIARY_UINT64_C(0xff00ff00u,0xff00ff00u));   // HH00FF00 DD00BB00
+	x = ((x >> 8) & TIARY_UINT64_C(0x00ff00ffu,0x00ff00ffu)) |  // 00GG00EE 00CC00AA
+		((x << 8) & TIARY_UINT64_C(0xff00ff00u,0xff00ff00u));   // HH00FF00 DD00BB00
 	return x;
 }
 
