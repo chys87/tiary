45c446c2008038ca7c1d1b41637af2359faab963
chys <admin@chys.info>
2014-07-02 20:30:57 +0800

    Kill _FORTIFY_SOURCE redefinition warning

---
+++ src/config-common.h
@@ -0,0 +1,15 @@
+/***********************************************************************
+ *
+ * Tiary, a terminal-based diary keeping system for Unix-like systems
+ * Copyright (C) 2009, 2010, 2011, 2012, 2014 chys <admin@CHYS.INFO>
+ *
+ * This software is licensed under the 3-clause BSD license.
+ * See LICENSE in the source package and/or online info for details.
+ *
+ **********************************************************************/
+
+// Some distributions have GCC predefine _FORTIFY_SOURCE
+#undef _FORTIFY_SOURCE
+#define _FORTIFY_SOURCE 2
+
+#define _FILE_OFFSET_BITS 64
